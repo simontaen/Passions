5124b9a955e54c50bbc83e61cb062be57b63399a
Simon Tännler <simon@mba-simon.fritz.box>
2014-09-03 11:37:08 +0200

    Advanced Parse setup with Push Notifications

--- Passions/PASAppDelegate.m
+++ Passions/PASAppDelegate.m
@@ -19,17 +19,35 @@
 	[LastFmFetchr fetchrWithApiKey:@"aed3367b0133ab707cb4e5b6b04da3e7"];
 	
 	// Setup Parse
-	[Parse setApplicationId:@"ymoWy7LcvcSg1tEGehR46hgLAEGP2mR3wyePOsQd"
-				  clientKey:@"Lp7MQjIGpM5O9Zp3CFyI8FU6NNmUBfa9xySu1Mgx"];
+	[Parse setApplicationId:@"nCQQ7cw92dCJJoH1cwbEv5ZBFmsEyFgSlVfmljp9"
+				  clientKey:@"Zpg8dAMm0f7IKpun9GTIRArCbgdY7e7Gj27vWPFc"];
 	
-	// Track app opens
-	[PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    [PFUser enableAutomaticUser];
+    
+    PFACL *defaultACL = [PFACL ACL];
+	
+    // If you would like all objects to be private by default, remove this line.
+    [defaultACL setPublicReadAccess:YES];
+    
+    [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
+	
+    if (application.applicationState != UIApplicationStateBackground) {
+        // Track an app open here if we launch with a push, unless
+        // "content_available" was used to trigger a background push (introduced
+        // in iOS 7). In that case, we skip tracking here to avoid double
+        // counting the app-open.
+        BOOL preBackgroundPush = ![application respondsToSelector:@selector(backgroundRefreshStatus)];
+        BOOL oldPushHandlerOnly = ![self respondsToSelector:@selector(application:didReceiveRemoteNotification:fetchCompletionHandler:)];
+        BOOL noPushPayload = ![launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+        if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+            //[PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+        }
+    }
 	
 	// Register for remote notifications
-	[application registerForRemoteNotificationTypes:
-	 UIRemoteNotificationTypeBadge|
-	 UIRemoteNotificationTypeAlert|
-	 UIRemoteNotificationTypeSound];
+	[application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+													 UIRemoteNotificationTypeAlert |
+													 UIRemoteNotificationTypeSound)];
 	
     return YES;
 }
@@ -38,13 +56,51 @@
 {
 	// Store the deviceToken in the current installation and save it to Parse.
 	PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-	[currentInstallation setDeviceTokenFromData:deviceToken];
-	[currentInstallation saveInBackground];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    currentInstallation.channels = @[@"global"];
+    [currentInstallation saveInBackground];
+
+	// From the starter project
+	[PFPush storeDeviceToken:deviceToken];
+    [PFPush subscribeToChannelInBackground:@"" target:self selector:@selector(subscribeFinished:error:)];
+
+}
+
+- (void)subscribeFinished:(NSNumber *)result error:(NSError *)error
+{
+    if ([result boolValue]) {
+        NSLog(@"ParseStarterProject successfully subscribed to push notifications on the broadcast channel.");
+    } else {
+        NSLog(@"ParseStarterProject failed to subscribe to push notifications on the broadcast channel.");
+    }
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    if (error.code == 3010) {
+        NSLog(@"Push notifications are not supported in the iOS Simulator.");
+    } else {
+        // show some alert or otherwise handle the failure to register.
+        NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
+	}
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
-	[PFPush handlePush:userInfo];
+    [PFPush handlePush:userInfo];
+	
+    if (application.applicationState == UIApplicationStateInactive) {
+        [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
+    }
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    [PFPush handlePush:userInfo];
+	
+    if (application.applicationState == UIApplicationStateInactive) {
+        [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
@@ -66,6 +122,11 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+	PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+	if (currentInstallation.badge != 0) {
+		currentInstallation.badge = 0;
+		[currentInstallation saveEventually];
+	}
 	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
