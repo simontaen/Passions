fac0a98cd3de19e202110e0c10c6d7536c2b75d7
Simon Tännler <simon@mba-simon.fritz.box>
2014-09-04 04:18:13 +0200

    next tasks

--- Passions/PASFavArtistsTVC.m
+++ Passions/PASFavArtistsTVC.m
@@ -97,11 +97,6 @@
     PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
 	[query whereKey:@"favByUsers" containsAllObjectsInArray:@[[PFUser currentUser]]];
 	
-    // If Pull To Refresh is enabled, query against the network by default.
-    if (self.pullToRefreshEnabled) {
-        query.cachePolicy = kPFCachePolicyNetworkOnly;
-    }
-	
     // If no objects are loaded in memory, we look to the cache first to fill the table
     // and then subsequently do a query against the network.
     if (self.objects.count == 0) {
@@ -139,18 +134,15 @@
 }
 
 
-
 /*
  // Override to customize the look of the cell that allows the user to load the next page of objects.
  // The default implementation is a UITableViewCellStyleDefault cell with simple labels.
  - (UITableViewCell *)tableView:(UITableView *)tableView cellForNextPageAtIndexPath:(NSIndexPath *)indexPath {
  static NSString *CellIdentifier = @"NextPage";
  
- UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+ // The cell should show a spinner and load more data automatically
  
- if (cell == nil) {
- cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
- }
+ UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
  
  cell.selectionStyle = UITableViewCellSelectionStyleNone;
  cell.textLabel.text = @"Load more...";
