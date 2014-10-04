d8dc5c0b1f9190d3808aa8c04d3cc798389ceb41
Simon Tännler <simon@mba-simon.fritz.box>
2014-10-04 22:07:17 +0200

    PASAlbum

---
+++ Passions/PASAlbum.h
@@ -0,0 +1,29 @@
+//
+//  PASAlbum.h
+//  Passions
+//
+//  Created by Simon Tännler on 04/10/14.
+//  Copyright (c) 2014 edgeguard. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface PASAlbum : PFObject<PFSubclassing>
+
+@property (nonatomic, strong) NSString* objectId;
+@property (nonatomic, strong) NSString* name;
+@property (nonatomic, strong) NSString* spotifyId;
+@property (nonatomic, strong) NSString* href;
+@property (nonatomic, strong) NSString* artistId;
+@property (nonatomic, strong) NSString* releaseDate;
+@property (nonatomic, strong) NSString* releaseDatePrecision;
+@property (nonatomic, strong) NSNumber* utc;
+@property (nonatomic, strong) NSArray* images; // of NSString
+@property (nonatomic, strong) NSDate* createdAt;
+@property (nonatomic, strong) NSDate* updatedAt;
+
+#pragma mark - Parse
+
++ (NSString *)parseClassName;
+
+@end
---
+++ Passions/PASAlbum.m
@@ -0,0 +1,38 @@
+//
+//  PASAlbum.m
+//  Passions
+//
+//  Created by Simon Tännler on 04/10/14.
+//  Copyright (c) 2014 edgeguard. All rights reserved.
+//
+
+#import "PASAlbum.h"
+#import <Parse/PFObject+Subclass.h>
+
+@implementation PASAlbum
+
+@dynamic objectId;
+@dynamic name;
+@dynamic spotifyId;
+@dynamic href;
+@dynamic artistId;
+@dynamic releaseDate;
+@dynamic releaseDatePrecision;
+@dynamic utc;
+@dynamic images; // of NSString
+@dynamic createdAt;
+@dynamic updatedAt;
+
+#pragma mark - Parse
+
++ (void)load
+{
+	[self registerSubclass];
+}
+
++ (NSString *)parseClassName
+{
+	return @"Album";
+}
+
+@end
