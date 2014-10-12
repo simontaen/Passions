c07a49e8c01aadefea61445e4c28522f54fa158c
Simon Tännler <simon@mba-simon.fritz.box>
2014-10-12 23:28:41 +0200

    Adding a XIB for ExtendedNavContainer for better layout, add the child view controller

--- Passions/PASExtendedNavContainer.m
+++ Passions/PASExtendedNavContainer.m
@@ -15,7 +15,7 @@ CGFloat const kSegmentBarHeight = 40;
 @interface PASExtendedNavContainer ()
 
 @property (nonatomic, strong) PASAddFromSamplesTVC *addTvc;
-@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 @property (weak, nonatomic) IBOutlet UIToolbar *segmentBar;
 @end
@@ -24,7 +24,7 @@ CGFloat const kSegmentBarHeight = 40;
 
 - (instancetype)initWithIndex:(NSUInteger)index
 {
-	self = [super init];
+	self = [super initWithNibName:nil bundle:nil];
 	if (!self) return nil;
 	_addTvc = [self _viewControllerAtIndex:index];
 	return self;
@@ -36,39 +36,38 @@ CGFloat const kSegmentBarHeight = 40;
 	
 	if ((isSimulator && index == 0) ||
 		(!isSimulator && index > 0)) {
-		return [[PASAddFromSamplesTVC alloc] initWithStyle:UITableViewStylePlain];
+		return [[PASAddFromSamplesTVC alloc] initWithNibName:nil bundle:nil];
 	} else {
-		return [[PASAddFromMusicTVC alloc] initWithStyle:UITableViewStylePlain];
+		return [[PASAddFromMusicTVC alloc] initWithNibName:nil bundle:nil];
 	}
 }
 
 #pragma mark - View Lifecycle
 
-- (void)loadView
-{
-	[super loadView];
-	
-	self.view = [[UIView alloc] initWithFrame:UIScreen.mainScreen.bounds];
-	
-	CGFloat myX = self.view.bounds.origin.x;
-	CGFloat myY = self.view.bounds.origin.y;
-	CGFloat myWith = self.view.bounds.size.width;
-	CGFloat myHeight = self.view.bounds.size.height;
-	
-	UIToolbar *myBar = [[UIToolbar alloc] initWithFrame:CGRectMake(myX, myY, myWith, kSegmentBarHeight)];
-	self.segmentBar = myBar;
-	[self.view addSubview:myBar];
-	
-	UISegmentedControl *mySegCont = [[UISegmentedControl alloc] initWithItems:@[@"alphabetical", @"by playcount"]];
-	self.segmentedControl.frame = CGRectMake(16, 8, myWith - 32, kSegmentBarHeight - 14);
-	self.segmentedControl = mySegCont;
-	[self.segmentBar addSubview:mySegCont];
-	
-	UITableView *myTable = [[UITableView alloc] initWithFrame:CGRectMake(myX, kSegmentBarHeight, myWith, myHeight)
-														style:UITableViewStylePlain];
-	self.myTableView = myTable;
-	[self.view addSubview:myTable];
-}
+//- (void)loadView
+//{
+//	[super loadView];
+//	
+//	self.view = [[UIView alloc] initWithFrame:UIScreen.mainScreen.bounds];
+//	
+//	CGFloat myX = self.view.bounds.origin.x;
+//	CGFloat myY = self.view.bounds.origin.y;
+//	CGFloat myWith = self.view.bounds.size.width;
+//	CGFloat myHeight = self.view.bounds.size.height;
+//	
+//	UIToolbar *myBar = [[UIToolbar alloc] initWithFrame:CGRectMake(myX, myY, myWith, kSegmentBarHeight)];
+//	self.segmentBar = myBar;
+//	[self.view addSubview:myBar];
+//	
+//	UISegmentedControl *mySegCont = [[UISegmentedControl alloc] initWithItems:@[@"alphabetical", @"by playcount"]];
+//	self.segmentedControl.frame = CGRectMake(16, 8, myWith - 32, kSegmentBarHeight - 14);
+//	self.segmentedControl = mySegCont;
+//	[self.segmentBar addSubview:mySegCont];
+//	
+//	UIView *myTable = [[UIView alloc] initWithFrame:CGRectMake(myX, kSegmentBarHeight, myWith, myHeight - kSegmentBarHeight)];
+//	self.containerView = myTable;
+//	[self.view addSubview:myTable];
+//}
 
 - (void)viewDidLoad
 {
@@ -78,13 +77,16 @@ CGFloat const kSegmentBarHeight = 40;
 	self.segmentedControl.contentHorizontalAlignment = UIControlContentHorizontalAlignmentFill;
 	self.segmentedControl.contentVerticalAlignment = UIControlContentVerticalAlignmentFill;
 	[self.segmentedControl addTarget:self action:@selector(segmentChanged:) forControlEvents:UIControlEventValueChanged];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-	[super viewDidAppear:animated];
 	
-	[PASResources printSubviews:self.segmentBar];
+	// add the child view controller
+	[self.addTvc.tableView setTranslatesAutoresizingMaskIntoConstraints:YES];
+	self.addTvc.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+	self.addTvc.tableView.frame = self.containerView.bounds;
+	
+	[self addChildViewController:self.addTvc];
+	[self.containerView addSubview:self.addTvc.view];
+	
+	[self.addTvc didMoveToParentViewController:self];
 }
 
 #pragma mark - Ordering
