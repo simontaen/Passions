5bef4c4b6e6a146f13125465175000c4943d152a
Simon Tännler <simon@mbp-simon.fritz.box>
2014-10-27 00:39:43 +0100

    adding the Apple way to hide the hairline

--- Passions/PASMyPVC.m
+++ Passions/PASMyPVC.m
@@ -53,6 +53,22 @@
 																		  target:self
 																		  action:@selector(doneButtonTapped:)];
 	self.navigationItem.rightBarButtonItem = rbbi;
+	
+	// For the extended navigation bar effect to work, a few changes
+	// must be made to the actual navigation bar.  Some of these changes could
+	// be applied in the storyboard but are made in code for clarity.
+	
+	// Translucency of the navigation bar is disabled so that it matches with
+	// the non-translucent background of the extension view.
+	[self.navigationController.navigationBar setTranslucent:NO];
+	
+	// The navigation bar's shadowImage is set to a transparent image.  In
+	// conjunction with providing a custom background image, this removes
+	// the grey hairline at the bottom of the navigation bar.  The
+	// ExtendedNavBarView will draw its own hairline.
+	[self.navigationController.navigationBar setShadowImage:[UIImage imageNamed:@"TransparentPixel"]];
+	// "Pixel" is a solid white 1x1 image.
+	[self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"Pixel"] forBarMetrics:UIBarMetricsDefault];	
 
 	// find the hairline below the navigationBar
 //	if (!self.navHairline) {
