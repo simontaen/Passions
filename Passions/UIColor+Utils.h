0e3b7b60a71028284afa86d8e6f6302b0a44fd98
Simon Tännler <simon@mbp-simon.local>
2014-10-27 11:10:43 +0100

    Color Utils

---
+++ Passions/UIColor+Utils.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Utils.h
+//  Passions
+//
+//  Created by Simon Tännler on 27/10/14.
+//  Copyright (c) 2014 edgeguard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Utils)
++ (UIColor *)defaultNavBarTintColor;
++ (UIColor *)musicNavBarTintColor;
++ (UIColor *)spotifyNavBarTintColor;
+@end
---
+++ Passions/UIColor+Utils.m
@@ -0,0 +1,52 @@
+//
+//  UIColor+Utils.m
+//  Passions
+//
+//  Created by Simon Tännler on 27/10/14.
+//  Copyright (c) 2014 edgeguard. All rights reserved.
+//
+
+#import "UIColor+Utils.h"
+
+@implementation UIColor (Utils)
++ (UIColor *)defaultNavBarTintColor
+{
+	static UIColor *defaultNavBarTintColor;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		defaultNavBarTintColor = [UIColor colorWithRed:(247/255.0) green:(247/255.0) blue:(247/255.0) alpha:1];
+	});
+	return defaultNavBarTintColor;
+}
+
++ (UIColor *)musicNavBarTintColor
+{
+	static UIColor *musicNavBarTintColor;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		musicNavBarTintColor = [UIColor orangeColor];
+	});
+	return musicNavBarTintColor;
+}
+
++ (UIColor *)spotifyNavBarTintColor
+{
+	static UIColor *spotifyNavBarTintColor;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		spotifyNavBarTintColor = [UIColor greenColor];
+	});
+	return spotifyNavBarTintColor;
+}
+
++ (UIColor *)starTintColor
+{
+	static UIColor *starTintColor;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		starTintColor = [UIColor yellowColor];
+	});
+	return starTintColor;
+}
+
+@end
