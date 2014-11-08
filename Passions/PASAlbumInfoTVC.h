68d83f56885ecda1baa415a9865093c0313fab91
Simon Tännler <simon@mbp-simon.fritz.box>
2014-11-08 13:53:56 +0100

    AlbumInfoTVC

---
+++ Passions/PASAlbumInfoTVC.h
@@ -0,0 +1,13 @@
+//
+//  PASAlbumInfoTVC.h
+//  Passions
+//
+//  Created by Simon Tännler on 08/11/14.
+//  Copyright (c) 2014 edgeguard. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PASAlbumInfoTVC : UITableViewController
+
+@end
---
+++ Passions/PASAlbumInfoTVC.m
@@ -0,0 +1,17 @@
+//
+//  PASAlbumInfoTVC.m
+//  Passions
+//
+//  Created by Simon Tännler on 08/11/14.
+//  Copyright (c) 2014 edgeguard. All rights reserved.
+//
+
+#import "PASAlbumInfoTVC.h"
+
+@interface PASAlbumInfoTVC ()
+
+@end
+
+@implementation PASAlbumInfoTVC
+
+@end
